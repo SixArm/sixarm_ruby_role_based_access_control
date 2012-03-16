e6dc6e676d0985b3702d05233b8e91f66331d9b9
SixArm <sixarm@sixarm.com>
2012-03-16 09:08:37 -0700

    Upgrade from test/unit to minitest/autorun

--- test/sixarm_ruby_role_based_access_control_test.rb
+++ test/sixarm_ruby_role_based_access_control_test.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
 SimpleCov.start
-require 'test/unit'
+require 'minitest/autorun'
 require 'sixarm_ruby_role_based_access_control'
 
 class RoleBasedAccessControlTest < Test::Unit::TestCase
