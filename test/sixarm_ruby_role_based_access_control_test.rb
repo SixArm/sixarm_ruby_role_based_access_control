2c09bdca6cdd6c86a83f2c575d70b8c74cb25ae2
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:53 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_role_based_access_control_test.rb
+++ test/sixarm_ruby_role_based_access_control_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_role_based_access_control'
+require "sixarm_ruby_role_based_access_control"
 
 class RoleBasedAccessControlTest < Test::Unit::TestCase
 
